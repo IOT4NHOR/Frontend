f1c2181c29f1db50deda5f189283558581b4aada
Phakaphol Dherachaisuphakij <godzk25@gmail.com>
2025-05-04 16:54:38 +0700

    fix bug

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,14 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {Routes , Route, BrowserRouter} from 'react-router-dom';
 import './index.css'
-import App from './App.jsx'
-import Dashboard from './components/dashboard.jsx';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App />} />
-      <Route path="/dashboard" element={<Dashboard />} />
     </Routes>
   </BrowserRouter>,
 )
